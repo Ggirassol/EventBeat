35f1abf0977e83e9612d7a3e7681dd52539bc7fe
Ggirassol <gi-12@hotmail.com>
2024-10-07 20:16:49 +0100

    more info about the single event displayed

--- src/components/SingleEvent/SingleEvent.jsx
+++ src/components/SingleEvent/SingleEvent.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getEventById } from "../../../api";
 import getDates from "../../getDatesfunc";
-import './singleEvent.css'
+import "./singleEvent.css";
 
 const SingleEvent = () => {
   const { event_id } = useParams();
@@ -18,13 +18,40 @@ const SingleEvent = () => {
   if (!singleEvent) {
     return <div>Loading...</div>;
   }
-    const eventDate = singleEvent.dates.start.localDate;
+  const eventDate = singleEvent.dates.start.localDate;
 
   return (
     <div className="single-event">
       <h1>{singleEvent.name}</h1>
-      <h3>{getDates(eventDate).weekday}, {getDates(eventDate).day} {getDates(eventDate).month} {getDates(eventDate).year}</h3>
-      <img src={singleEvent.images[8].url}/>
+      <h3>
+        {getDates(eventDate).weekday}, {getDates(eventDate).day}{" "}
+        {getDates(eventDate).month} {getDates(eventDate).year}
+      </h3>
+      <p>{singleEvent._embedded.venues[0].name}</p>
+      <p className="bold">{singleEvent.dates.start.localTime.slice(0, -3)}</p>
+      <img src={singleEvent.images[8].url} />
+      <p className="genre">
+        {singleEvent.classifications[0].segment.name !== "Undefined" && (
+          <span>{singleEvent.classifications[0].segment.name} </span>
+        )}
+        {singleEvent.classifications[0].genre.name !== "Undefined" && (
+          <span>{singleEvent.classifications[0].genre.name} </span>
+        )}
+        {singleEvent.classifications[0].subGenre.name !== "Undefined" && (
+          <span>{singleEvent.classifications[0].subGenre.name} </span>
+        )}
+        {singleEvent.classifications[0].subType.name !== "Undefined" && (
+          <span>{singleEvent.classifications[0].subType.name} </span>
+        )}
+      </p>
+      <p className="adress bold">Adress:</p>
+      {Object.entries(singleEvent._embedded.venues[0].address).map(
+        ([key, value]) => (
+          <p key={key}>{value}</p>
+        )
+      )}
+      <p>{singleEvent._embedded.venues[0].city.name}</p>
+      <button>Sign Up</button>
     </div>
   );
 };
