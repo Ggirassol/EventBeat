0594dca77ff934f7bd1fa2de062fd9125e76903b
Ggirassol <gi-12@hotmail.com>
2024-10-25 21:25:32 +0100

    fixing router bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import { UserProvider } from "../UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <UserProvider>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </UserProvider>
 );
